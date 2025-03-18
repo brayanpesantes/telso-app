3fd82c2fadf3304258ce9b1f1470cdb7ed19a703
brayanpesantes <brayalexis.30@gmail.com>
2025-03-17 23:18:32 -0500

    Add Sidebar component and integrate it into the layout

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,9 +1,10 @@
-import { TopMenu } from "@/components";
+import { Sidebar, TopMenu } from "@/components";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-svh px-5">
       <TopMenu />
+      <Sidebar />
       <div className="px-0 sm:px-10">{children}</div>
     </main>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./ui/top-menu";
 export * from "./ui/not-found";
 export * from "./ui/title";
+export * from "./ui/sidebar";
 
 export * from "./products/product-grid";
 export * from "./products/product-grid-item";
