8144bc2a73ca15240f1b5adf0bb5d70b05623241
brayanpesantes <brayalexis.30@gmail.com>
2025-03-19 23:59:37 -0500

    Add gender-based product page and not found component; remove old category page

---
+++ src/app/(shop)/gender/[gender]/page.tsx
@@ -0,0 +1,40 @@
+import { getPaginationProductWithImages } from "@/actions";
+import { Pagination, ProductGrid, Title } from "@/components";
+import { Gender } from "@prisma/client";
+import { notFound, redirect } from "next/navigation";
+
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+type Params = Promise<{
+  gender: Gender;
+}>;
+
+export default async function Page(props: {
+  searchParams: SearchParams;
+  params: Params;
+}) {
+  const { gender } = await props.params;
+  if (!Object.values(Gender).includes(gender as Gender)) {
+    notFound();
+  }
+
+  const searchParams = await props.searchParams;
+  const page = searchParams.page ? parseInt(searchParams.page as string) : 1;
+
+  const { products, currentPage, totalPages } =
+    await getPaginationProductWithImages({ page, gender });
+
+  if (products.length === 0) {
+    redirect(`/gender/${gender}`);
+  }
+  return (
+    <>
+      <Title
+        title={`Productos de ${gender}`}
+        subTitle={`Todos los Productos`}
+        className="mb-2"
+      />
+      <ProductGrid products={products} />
+      <Pagination totalPages={totalPages} />
+    </>
+  );
+}
