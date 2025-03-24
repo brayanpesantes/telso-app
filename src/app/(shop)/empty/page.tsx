a3e4c42d7d7a4791936e77a0e6671ae6f19398eb
brayanpesantes <brayalexis.30@gmail.com>
2025-03-23 23:09:58 -0500

    Add empty cart page with message and link to return to shop

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
+
 export default function Page() {
-    return (
-        <div></div>
-    );
+  return (
+    <div className="flex justify-center items-center min-h-svh">
+      <IoCartOutline size={80} className="mx-5" />
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">Tu carrito está vació</h1>
+        <Link href="/" className="text-blue-500 mt-2 text-4xl">
+          Regresar
+        </Link>
+      </div>
+    </div>
+  );
 }
