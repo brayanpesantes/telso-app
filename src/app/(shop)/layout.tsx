be297a2e1a8473bbf5bbe27ee00f549e2a8a65b2
brayanpesantes <brayalexis.30@gmail.com>
2025-03-23 23:09:29 -0500

    Add footer component and integrate it into the layout; update component exports

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar, TopMenu } from "@/components";
+import { Footer, Sidebar, TopMenu } from "@/components";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -6,6 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <TopMenu />
       <Sidebar />
       <div className="px-0 sm:px-10">{children}</div>
+      <Footer />
     </main>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,14 @@
-export * from "./ui/top-menu";
+export * from "./ui/footer";
 export * from "./ui/not-found";
-export * from "./ui/title";
-export * from "./ui/sidebar";
 export * from "./ui/pagination";
+export * from "./ui/sidebar";
+export * from "./ui/title";
+export * from "./ui/top-menu";
 
 export * from "./products/product-grid";
 export * from "./products/product-grid-item";
 
-export * from "./product/size-selector";
-export * from "./product/quantity-selector";
-export * from "./product/product-slide-show";
 export * from "./product/product-mobile-slide-show";
+export * from "./product/product-slide-show";
+export * from "./product/quantity-selector";
+export * from "./product/size-selector";
