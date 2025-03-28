c51273f53056d37278ee3321a5b0093f3d0ddae7
brayanpesantes <brayalexis.30@gmail.com>
2025-03-28 00:12:57 -0500

    Implement authentication functionality with login form and NextAuth integration

---
+++ src/auth.config.ts
@@ -0,0 +1,40 @@
+import type { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
+import bcryptjs from "bcryptjs";
+
+export const authConfig: NextAuthConfig = {
+  pages: {
+    signIn: "/auth/login",
+    newUser: "/auth/new-account",
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) {
+          return null;
+        }
+        const { email, password } = parsedCredentials.data;
+        const user = await prisma.user.findUnique({
+          where: { email: email.toLowerCase() },
+        });
+        if (!user) return null;
+
+        const isValidPassword = await bcryptjs.compare(password, user.password);
+        if (!isValidPassword) return null;
+
+        const { password: _, ...rest } = user;
+
+        return rest;
+      },
+    }),
+  ],
+};
+
+export const { auth: middleware, signIn, signOut } = NextAuth(authConfig);
