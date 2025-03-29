e6e5907fbf9b2e293c2ef34a9462ad925f0a8fa4
brayanpesantes <brayalexis.30@gmail.com>
2025-03-29 00:40:26 -0500

    feat(auth): enhance login form with pending state and error handling
    Add a disabled button state during form submission and display error messages for invalid credentials. Improve form action handling and add a callback URL for redirection after login.

--- src/app/auth/login/ui/login-form.tsx
+++ src/app/auth/login/ui/login-form.tsx
@@ -1,12 +1,22 @@
 "use client";
-import Link from "next/link";
-import { useFormState } from "react-dom";
+
 import { authenticate } from "@/actions";
+import clsx from "clsx";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useActionState } from "react";
+import { IoInformationOutline } from "react-icons/io5";
 
 export const LoginForm = () => {
-  const [state, dispatch] = useFormState(authenticate, undefined);
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callback") || "/dashaborad";
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined
+  );
+
   return (
-    <form action={dispatch} className="flex flex-col">
+    <form action={formAction} className="flex flex-col">
       <label htmlFor="email">Correo electrónico</label>
       <input
         className="px-5 py-2 border bg-gray-200 rounded mb-5"
@@ -22,8 +32,16 @@ export const LoginForm = () => {
         name="password"
         required
       />
+      <input type="hidden" name="redirecto" value={callbackUrl} />
 
-      <button className="btn-primary" type="submit">
+      <button
+        className={clsx({
+          "btn-primary": !isPending,
+          "btn-disabled": isPending,
+        })}
+        type="submit"
+        aria-disabled={isPending}
+      >
         Ingresar
       </button>
 
@@ -31,8 +49,16 @@ export const LoginForm = () => {
       <div className="flex items-center my-5">
         <div className="flex-1 border-t border-gray-500"></div>
         <div className="px-2 text-gray-800">O</div>
-        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="flex-1 border-t bordecr-gray-500"></div>
       </div>
+      {errorMessage === "Invalid credentials." && (
+        <div className="mb-2 inline-flex">
+          <IoInformationOutline className="h-5 w-5 text-red-500" />
+          <p className="text-sm text-red-500">
+            Creadenciales no son Correctas.
+          </p>
+        </div>
+      )}
 
       <Link href="/auth/new-account" className="btn-secondary text-center">
         Crear una nueva cuenta
