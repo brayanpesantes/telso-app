cceb2681ac38f8528270f6c8c009a9e69cb88d9d
brayanpesantes <brayalexis.30@gmail.com>
2025-04-10 00:32:24 -0500

    refactor(middleware): simplify authentication logic using NextAuth
    The authentication middleware was refactored to leverage NextAuth's built-in functionality, reducing custom logic and improving maintainability. This change replaces the manual handling of authentication checks and redirects with NextAuth's `auth` method.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,7 @@
-import { auth } from "@/auth.config";
+import { authConfig } from "@/auth.config";
+import NextAuth from "next-auth";
 
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  const isAuthPage = req.nextUrl.pathname.startsWith("/auth");
-  const isProfilePage = req.nextUrl.pathname.startsWith("/profile");
-  const isHomePage = req.nextUrl.pathname === "/";
-  const isPublicPage =
-    isHomePage || req.nextUrl.pathname.startsWith("/products");
-
-  // Si es una página pública, permitimos el acceso
-  if (isPublicPage) return;
-
-  // Si el usuario no está autenticado y trata de acceder a una ruta protegida
-  if (!isLoggedIn && isProfilePage) {
-    const loginUrl = new URL("/auth/login", req.nextUrl);
-    loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
-    return Response.redirect(loginUrl);
-  }
-
-  // Si el usuario está autenticado y trata de acceder a páginas de auth
-  if (isLoggedIn && isAuthPage) {
-    return Response.redirect(new URL("/profile", req.nextUrl));
-  }
-});
+export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
