85ee50b64ef059d4ffde08a989b677592b2c7ec5
brayanpesantes <brayalexis.30@gmail.com>
2025-04-16 18:41:12 -0500

    feat(product): integrar Cloudinary para la carga de imágenes
    Se añade la configuración de Cloudinary en el archivo de configuración de Next.js y se implementa la función `uploadImages` en la acción `createUpdateProduct` para manejar la carga de imágenes desde el formulario de producto. Esto permite la subida de imágenes a la nube, mejorando la gestión de recursos multimedia en la aplicación.

--- src/actions/products/create-update-producto.ts
+++ src/actions/products/create-update-producto.ts
@@ -2,9 +2,10 @@
 
 import { prisma } from "@/lib/prisma";
 import { Gender, Product, Size } from "@prisma/client";
+import { v2 as cloudinary } from "cloudinary";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
-
+cloudinary.config(process.env.CLOUDINARY_URL ?? "");
 const productSchema = z.object({
   id: z.string().uuid().optional().nullable(),
   title: z.string().min(3).max(255),
@@ -64,7 +65,16 @@ export const createUpdateProduct = async (formData: FormData) => {
         });
       }
       if (formData.getAll("images")) {
-        console.log(formData.getAll("images"));
+        const images = await uploadImages(formData.getAll("images") as File[]);
+        if (!images) {
+          throw new Error("No se pudo crear las imágenes");
+        }
+        await prisma.productImage.createMany({
+          data: images.map((image) => ({
+            url: image!,
+            productId: product.id,
+          })),
+        });
       }
       return {
         product,
@@ -80,5 +90,28 @@ export const createUpdateProduct = async (formData: FormData) => {
       ok: false,
       message: "Revisar los logs, no se pudo actualizar/crear",
     };
+  }
+};
+
+const uploadImages = async (images: File[]) => {
+  try {
+    const uploadPromises = images.map(async (image) => {
+      try {
+        const buffer = await image.arrayBuffer();
+        const base64Image = Buffer.from(buffer).toString("base64");
+        return cloudinary.uploader
+          .upload(`data:image/png;base64,${base64Image}`)
+          .then((r) => r.secure_url);
+      } catch (error) {
+        console.log(error);
+
+        return null;
+      }
+    });
+    const uploadImages = await Promise.all(uploadPromises);
+    return uploadImages;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 };
